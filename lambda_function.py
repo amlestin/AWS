3b21a5147f97f6835785569e0603db0747b6425a
Arnold Lestin <amlestin@gmail.com>
2021-08-05 20:27:29 -0400

    Input validation

--- lambda_function.py
+++ lambda_function.py
@@ -7,6 +7,10 @@ logger.setLevel(logging.DEBUG)
 
 def lambda_handler(event, context):
     route = int(event)
+    # input validation
+    if route < 0:
+        return "Invalid Input"
+
     response = requests.get(F'https://www.usfbullrunner.com/Route/{route}/')
 
     posts = json.loads(response.text)
